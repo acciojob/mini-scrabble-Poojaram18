1941dfaf6f74030f01af6d3327015af2811bd367
PoojaRamaraj <119429335+Poojaram18@users.noreply.github.com>
2023-01-12 18:57:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your code here
+
+var textArea = document.getElementById('evaluatedText');
+var count = document.getElementById('letterCount');
+
+textArea.addEventListener('keyup', function(){
+    let text = textArea.value;
+    // text = removeSpaces(text);
+
+    count.innerText = text.length;
+})
+
